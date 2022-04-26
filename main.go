a3ee7d1c546f0ca792b990167e7e341175d64718
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-26 17:55:36 +0430

    api init

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
-
 func main() {
-
-	url := "https://api-nba-v1.p.rapidapi.com/seasons"
-
-	req, _ := http.NewRequest("GET", url, nil)
-
-	req.Header.Add("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com")
-	req.Header.Add("X-RapidAPI-Key", "d9ebf01ef3mshb3ed3ad38d37daep179ba1jsn50dda0c0097d")
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
 
 }
