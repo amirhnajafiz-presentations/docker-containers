d5180f50533f3f483c9894ba62b38ee1e34ad165
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-29 11:27:59 +0430

    bot new method added

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,8 +1,17 @@
 package bot
 
+import "gopkg.in/telebot.v3"
+
 type Bot struct {
 }
 
-func New() {
+func New() (*telebot.Bot, error) {
+	b, err := telebot.NewBot(Config())
+	if err != nil {
+		return nil, err
+	}
 
+	// TODO: register handler
+
+	return b, nil
 }
