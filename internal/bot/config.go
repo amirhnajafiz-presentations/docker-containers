39a1d42866e8b1d00f189feadba0eac46058a1f8
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-29 11:26:14 +0430

    bot configs method implemented

--- internal/bot/config.go
+++ internal/bot/config.go
@@ -1,5 +1,17 @@
 package bot
 
-func Config() {
+import (
+	"os"
+	"time"
 
+	"gopkg.in/telebot.v3"
+)
+
+func Config() telebot.Settings {
+	return telebot.Settings{
+		Token: os.Getenv("token"),
+		Poller: &telebot.LongPoller{
+			Timeout: 10 * time.Second,
+		},
+	}
 }
