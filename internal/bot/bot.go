8217d731e64988cca6a16c1b3fc0ab973829b1f7
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-29 11:24:19 +0430

    bot config init

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,38 +1,8 @@
 package bot
 
-import (
-	"log"
-
-	telBot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-)
-
 type Bot struct {
-	Token string
 }
 
-func (b Bot) Start() {
-	bot, err := telBot.NewBotAPI(b.Token)
-	if err != nil {
-		log.Panic(err)
-	}
+func New() {
 
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := telBot.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := telBot.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			_, _ = bot.Send(msg)
-		}
-	}
 }
