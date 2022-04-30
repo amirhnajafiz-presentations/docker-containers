ba37b8d7e050bfba3c06ea4df554d42429eeb6c3
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-30 16:38:57 +0430

    new metrics method implemented

--- internal/handler/metric.go
+++ internal/handler/metric.go
@@ -23,3 +23,29 @@ func newCounter(counterOpts prometheus.CounterOpts) prometheus.Counter {
 
 	return ev
 }
+
+func NewMetrics() Metrics {
+	return Metrics{
+		ConnectionErrors: newCounter(prometheus.CounterOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "connection_errors_total",
+			Help:        "total number of connection errors",
+			ConstLabels: nil,
+		}),
+		SuccessfulRequest: newCounter(prometheus.CounterOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "successful_request_total",
+			Help:        "total number of successful requests",
+			ConstLabels: nil,
+		}),
+		FailedRequest: newCounter(prometheus.CounterOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "failed_request_total",
+			Help:        "total number of failed requests",
+			ConstLabels: nil,
+		}),
+	}
+}
