c9b30782f3cfe02afbca0787407e21fa2431f746
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-30 16:30:56 +0430

    metric server init

--- internal/metric/metric.go
+++ internal/metric/metric.go
@@ -1,1 +1,25 @@
 package metric
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Server struct {
+	srv     *http.ServeMux
+	address string
+}
+
+// NewServer creates a new monitoring server.
+func NewServer(cfg Config) Server {
+	var srv *http.ServeMux
+
+	srv = http.NewServeMux()
+	srv.Handle("/metrics", promhttp.Handler())
+
+	return Server{
+		address: cfg.Host,
+		srv:     srv,
+	}
+}
