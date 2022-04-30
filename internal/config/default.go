e22bef924f7ab7328d7c06caf39bb296a7b7f61b
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-30 16:24:47 +0430

    bot config added

--- internal/config/default.go
+++ internal/config/default.go
@@ -1,5 +1,11 @@
 package config
 
+import "github.com/amirhnajafiz/nba-bot/internal/bot"
+
 func Default() Config {
-	return Config{}
+	return Config{
+		Bot: bot.Config{
+			Token: "",
+		},
+	}
 }
