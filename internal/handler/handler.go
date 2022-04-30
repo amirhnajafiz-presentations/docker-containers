947f38ddb61dc88b36e20001c56f8cd4c4eaf2aa
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-30 16:40:08 +0430

    handler metrics connected

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,8 +5,11 @@ import (
 )
 
 type Handler struct {
+	Metric Metrics
 }
 
 func (h Handler) Test(context telebot.Context) error {
+	h.Metric.SuccessfulRequest.Add(1)
+
 	return context.Send("Success")
 }
